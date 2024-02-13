c881a9a4a529e5f47932c9af97d20a54d88f9a39
Aleksandr Berezhnoy <alex@MacBook-Pro-Aleksandr.local>
2024-02-13 20:39:28 +0300

    homework001 added

---
+++ API Operations History/src/main/java/ru/netology/aberezhnoy/Main.java
@@ -0,0 +1,10 @@
+package ru.netology.aberezhnoy;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Aleksandr Berezhnoy";
+        for (int i = 0; i < 3; i++) {
+            System.out.println(name);
+        }
+    }
+}

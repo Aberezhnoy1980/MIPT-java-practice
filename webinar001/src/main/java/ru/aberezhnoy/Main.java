7cbf127bc353cf439f81407e9d8321340156903c
Aleksandr Berezhnoy <alex@MacBook-Pro-Aleksandr.local>
2024-02-13 20:31:06 +0300

    webinar001 added

---
+++ webinar001/src/main/java/ru/aberezhnoy/Main.java
@@ -0,0 +1,11 @@
+package ru.aberezhnoy;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome");
+
+        for (int i = 0; i < 15; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}

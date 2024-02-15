fe4ac6c2abb227aac4ece8b05a55f74694bb9083
Aleksandr Berezhnoy <alex@MacBook-Pro-Aleksandr.local>
2024-02-15 05:48:02 +0300

    homework002 added

--- API Operations History/src/main/java/ru/netology/aberezhnoy/Main.java
+++ API Operations History/src/main/java/ru/netology/aberezhnoy/Main.java
@@ -1,10 +1,17 @@
 package ru.netology.aberezhnoy;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        String name = "Aleksandr Berezhnoy";
-        for (int i = 0; i < 3; i++) {
-            System.out.println(name);
-        }
+
+        int id = sc.nextInt();
+        double amount = sc.nextDouble();
+        LocalDate transactionDate = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+        System.out.println("Transaction id: " + id + ", amount: " + amount + ", date:  " + transactionDate);
     }
 }

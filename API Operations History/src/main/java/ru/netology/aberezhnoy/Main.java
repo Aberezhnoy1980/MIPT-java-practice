6f321b2f9d2a3fe0ce5fc993a50082bdd6950c47
Aleksandr Berezhnoy <alex@MacBook-Pro-Aleksandr.local>
2024-02-21 17:54:25 +0300

    homework003 done

--- API Operations History/src/main/java/ru/netology/aberezhnoy/Main.java
+++ API Operations History/src/main/java/ru/netology/aberezhnoy/Main.java
@@ -2,16 +2,47 @@ package ru.netology.aberezhnoy;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     private static final Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        int id = sc.nextInt();
-        double amount = sc.nextDouble();
-        LocalDate transactionDate = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        final ArrayList<Transaction> transactions = new ArrayList<>();
+        int i = 0;
+        while (i != 5) {
+            i++;
+            System.out.print("Please, enter any integer for id: ");
+            int outerId = sc.nextInt();
 
-        System.out.println("Transaction id: " + id + ", amount: " + amount + ", date:  " + transactionDate);
+            System.out.print("Please, enter any double for amount (use ',' as delimiter): ");
+            double outerAmount = sc.nextDouble();
+
+            System.out.print("Please, enter any date in format dd/MM/yyyy for date: ");
+            LocalDate outerDate = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            transactions.add(new Transaction(outerId, outerAmount, outerDate));
+        }
+
+        System.out.println(transactions);
     }
+
+    public static class Transaction {
+        private final int id;
+        private final double amount;
+        private final LocalDate date;
+
+        public Transaction(int id, double amount, LocalDate date) {
+            this.id = id;
+            this.amount = amount;
+            this.date = date;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("\nTransaction id: %d, amount: %s, date: %s", id, amount, date);
+        }
+    }
+
 }
